92d12ca1c82b29fa9d3f513a2757ab20e82ffdbf
new-user <new-user@pm.me>
2024-10-07 05:40:41 +0530

    compress

---
+++ compress.py
@@ -0,0 +1,17 @@
+from PIL import Image as im
+
+def compress_image(file_path, output_directory, compression_quality=75):
+    
+    try:
+        with im.open(file_path) as image:
+            original_size = file_path.stat().st_size  
+            output_path = output_directory / file_path.name
+            image.save(output_path, optimize=True, quality=compression_quality)  # custom qualitty
+            
+            compressed_size = output_path.stat().st_size  # size in bytes
+            
+            compression_percentage = (1 - compressed_size / original_size) * 100
+            
+            print(f"Compressed {file_path} to {output_path}. Compression: {compression_percentage:.2f}%")
+    except Exception as e:
+        print(f"Error compressing {file_path}: {e}")
